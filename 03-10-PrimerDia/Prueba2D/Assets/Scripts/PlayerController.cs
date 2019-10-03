b9e09693bfe4837cf34aa0ef7e24df494dc20a32
Juan Francisco Benítez López <juanfbenitezlopez@gmail.com>
2019-10-03 20:10:04 +0200

    Primer dia de clase

---
+++ 03-10-PrimerDia/Prueba2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxSpeed = 10f;
+    public KeyCode leftMovement, rightMovement;
+    private bool _facingRight = true;
+
+    void Update()
+    {
+        CheckMovement();
+    }
+
+    private void CheckMovement()
+    {
+        if((Input.GetKey(leftMovement) && _facingRight) || (Input.GetKey(rightMovement) && !_facingRight))
+        {
+            _facingRight = !_facingRight;
+            transform.Rotate(Vector3.up * 180f);
+        }
+
+        if(Input.GetKey(leftMovement) || Input.GetKey(rightMovement))
+            transform.Translate(Vector3.right * maxSpeed * Time.deltaTime);
+    }
+}
---
+++ 03-10-PrimerDia/Prueba2D/Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject objToSpawn; // Objeto a instanciar
+    public float spawnTime; // Tiempo entre instanciaciones
+	private float _timer;
+
+    void Update()
+    {
+		_timer -= Time.deltaTime;
+		if(_timer <= 0)
+		{
+			SpawnObj();
+			_timer = spawnTime;
+		}
+    }
+
+    public void SpawnObj()
+    {
+        Instantiate(objToSpawn, transform.position, transform.rotation); // Crea el objeto en la posición y con la rotación que tenga el objeto que contiene el script.
+    }
+}
