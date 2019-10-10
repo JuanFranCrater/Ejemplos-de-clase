a4e30023dde032c803c251bcd3228529b4dff9dc
JuanFranCrater <juanfbenitezlopez@gmail.com>
2019-10-10 17:53:51 +0200

    Pues disparitos y roboces

---
+++ Intro2_3D/Assets/Scripts/CharacterController3D.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController3D : MonoBehaviour
+{
+    [Header("Movement")]
+    public string horizontalAxis;
+    public string verticalAxis;
+    public float moveSpeed;
+    public float rotateSpeed;
+
+    private Rigidbody _rb;
+    private Animator _anim;
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw(horizontalAxis);
+        float v = Input.GetAxisRaw(verticalAxis);
+
+        CheckMove(h, v);
+    }
+
+    private void CheckMove(float h, float v)
+    {
+        if (h != 0 || v != 0)
+        {
+            Rotate(h, v);
+            transform.Translate(new Vector3(0, 0, Time.deltaTime * moveSpeed));
+            if(_anim)
+                _anim.SetBool("Walk", true); 
+        }
+        else if(_anim)
+            _anim.SetBool("Walk", false);
+    }
+
+    private void Rotate(float h, float v)
+    {
+        Vector3 targetDirection = new Vector3(h, 0f, v);
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+
+        _rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime));
+    }    
+}
---
+++ Intro2_3D/Assets/Scripts/Killer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Killer : MonoBehaviour
+{
+    public string killerTag;
+    public float deathDelay;
+    private Animator _anim;
+    private bool _killed;
+
+    private void Awake()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == killerTag)
+        {
+            Destroy(col.gameObject);
+
+            if(_anim && !_killed)
+                _anim.SetTrigger("Die");
+            Destroy(gameObject, deathDelay);
+
+            _killed = true;
+        }
+    }
+}
