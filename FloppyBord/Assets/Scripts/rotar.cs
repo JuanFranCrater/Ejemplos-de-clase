d263eb00b2d640926c7c6fca88604bec2889fe2f
JuanFranCrater <juanfbenitezlopez@gmail.com>
2019-10-24 18:58:22 +0200

    Pajaros

---
+++ FloppyBord/Assets/Scripts/rotar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotar : MonoBehaviour
+{
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0, 0, Time.deltaTime * speed, Space.Self);
+    }
+}
